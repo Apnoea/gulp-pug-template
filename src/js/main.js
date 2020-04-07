405a7d95830b267a0acb8e0dc3ce33f47e6ccfbf
Alex <36691208+Apnoea@users.noreply.github.com>
2020-04-07 09:53:03 +0300

    update packages, minor fixes

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,14 @@ import Swiper from "swiper";
 $(document).ready(function() {
   var mySwiper = new Swiper('.swiper-container', {
     speed: 400,
-    spaceBetween: 100
+    spaceBetween: 100,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
 });
