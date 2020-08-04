366a1333654ed17c9fc6f2727530259212b483ca
Alex <36691208+Apnoea@users.noreply.github.com>
2020-08-04 10:12:25 +0300

    update v0.1.0
    - add incremenal pug build
    - minor fixes

--- gulp-tasks/pug2html.js
+++ gulp-tasks/pug2html.js
@@ -3,11 +3,29 @@ const gulp = require('gulp'),
   pugLinter = require('gulp-pug-linter'),
   filter = require('gulp-filter'),
   pug = require('gulp-pug'),
-  htmlValidator = require('gulp-w3c-html-validator')
+  cached = require('gulp-cached'),
+  dependents = require('gulp-dependents'),
+  htmlValidator = require('gulp-w3c-html-validator');
 
 module.exports = function pug2html() {
   return gulp.src('src/pages/**/*.pug')
     .pipe(plumber())
+    .pipe(cached('pugCache'))
+    .pipe(dependents({
+      ".pug": {
+        parserSteps: [
+          /^\s*(?:extends|include)\s+(.+?)\s*$/gm,
+          function (str) {
+            var absolute = str.match(/^[\\/]+(.+)/);
+            if (absolute) {
+              str = path.resolve('src/pages', absolute[1]);
+            }
+            return [str];
+          }
+        ],
+        postfixes: ['.pug', '.jade']
+      }
+    }))
     .pipe(pugLinter({ reporter: 'default' }))
     .pipe(filter('src/pages/*.pug'))
     .pipe(pug({ pretty: true }))
