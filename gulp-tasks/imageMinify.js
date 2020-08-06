4c14208b648df78772f993dc0fe018fbc640c25b
Alex <36691208+Apnoea@users.noreply.github.com>
2020-08-06 15:09:50 +0300

    update v0.1.3
    - upd png optimization
    - upd readme

--- gulp-tasks/imageMinify.js
+++ gulp-tasks/imageMinify.js
@@ -1,17 +1,15 @@
 const gulp = require('gulp'),
   changed = require('gulp-changed'),
-  imagemin = require('gulp-imagemin')
+  imagemin = require('gulp-imagemin'),
+  imageminPngquant = require('imagemin-pngquant')
 
 module.exports = function imageMinify() {
   return gulp.src('src/images/**/*.{gif,png,jpg,webp}')
     .pipe(changed('build/img'))
     .pipe(imagemin([
+      imageminPngquant({ quality: [0.75, 0.75] }),
       imagemin.gifsicle({ interlaced: true }),
-      imagemin.mozjpeg({
-        quality: 75,
-        progressive: true
-      }),
-      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.mozjpeg({ quality: 75, progressive: true })
     ]))
     .pipe(gulp.dest('build/img'))
 }
