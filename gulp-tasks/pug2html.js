b621e05b492a2d00432c6bb81be808deeb501690
Alex K <oknetuzok@gmail.com>
2020-11-23 13:37:12 +0300

    fix pug2html compile
    - fixes

--- gulp-tasks/pug2html.js
+++ gulp-tasks/pug2html.js
@@ -1,37 +1,15 @@
 const gulp = require('gulp'),
   plumber = require('gulp-plumber'),
   pugLinter = require('gulp-pug-linter'),
+  filter = require('gulp-filter'),
   pug = require('gulp-pug'),
   cached = require('gulp-cached'),
   dependents = require('gulp-dependents'),
   htmlValidator = require('gulp-w3c-html-validator'),
   browsersync = require('browser-sync')
 
-function pugLint() {
-  return gulp.src('src/pages/**/*.pug')
-    .pipe(plumber())
-    .pipe(cached('pugLintCache'))
-    .pipe(dependents({
-      ".pug": {
-        parserSteps: [
-          /^\s*(?:extends|include)\s+(.+?)\s*$/gm,
-          function (str) {
-            var absolute = str.match(/^[\\/]+(.+)/);
-            if (absolute) {
-              str = path.resolve('src/pages', absolute[1]);
-            }
-            return [str];
-          }
-        ],
-        postfixes: ['.pug', '.jade']
-      }
-    }))
-    .pipe(pugLinter({ reporter: 'default' }))
-}
-
 module.exports = function pug2html() {
-  pugLint()
-  return gulp.src('src/pages/*.pug')
+  return gulp.src('src/pages/**/*.pug')
     .pipe(plumber())
     .pipe(cached('pugCache'))
     .pipe(dependents({
@@ -49,8 +27,10 @@ module.exports = function pug2html() {
         postfixes: ['.pug', '.jade']
       }
     }))
+    .pipe(pugLinter({ reporter: 'default' }))
+    .pipe(filter('src/pages/*.pug'))
     .pipe(pug({ pretty: true }))
     .pipe(htmlValidator())
     .pipe(gulp.dest('build'))
-    .pipe(browsersync.stream());
+    .pipe(browsersync.stream())
 }
